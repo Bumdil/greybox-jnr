ef299f271d92bb28db88091856d321b5e64c369f
Georg Hagemann <hagemann@cip.ifi.lmu.de>
2019-04-26 10:50:59 +0200

    Create Unity Project and add basic level components

---
+++ Assets/Scripts/LevelObjects/RotationController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationController : MonoBehaviour {
+
+    public bool isClockwise = true;
+    private float clockwiseMultiplier = 0f;
+
+	// Add a negative 1 multiplier to rotation for counter clockwise
+	void Start () {
+        clockwiseMultiplier = (isClockwise ? -1f : 1f);
+	}
+	
+	// Each second, rotate by 45 degrees.
+	void Update () {
+        clockwiseMultiplier = (isClockwise ? -1f : 1f); // check each frame as this may be manipulated.
+        this.transform.Rotate(new Vector3(0, 0, 45) * clockwiseMultiplier * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/Player/InputController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour {
+
+    [Range(0,100)]
+    public float speed = 15;
+    [Range(0, 100)]
+    public float jumpHeight = 30;
+    private bool isOnGround;
+    private BoxCollider trigger;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        trigger = GetComponent<BoxCollider>();
+        rb = GetComponent<Rigidbody>();
+        isOnGround = true;
+    }
+
+    private void Update()
+    {
+
+        if (Input.GetAxis("Jump") > 0 && isOnGround)
+        {
+            //jump
+            rb.AddForce(new Vector3(0, Input.GetAxis("Jump") * jumpHeight * 20, 0));
+            //Debug.Log("Jump!!");
+        }
+        else if (Input.GetAxis("Horizontal") != 0 && isOnGround)
+        {
+            //go right/left
+            rb.velocity = new Vector3(Input.GetAxis("Horizontal") * speed, rb.velocity.y, 0);
+            //Debug.Log("Input: " + Input.GetAxis("Horizontal"));
+        }
+        if (Input.GetAxis("Use") > 0)
+        {
+            //use object
+        }
+        if (Input.GetAxis("Restart") > 0)
+        {
+            //restart level
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Ground")
+        {
+            isOnGround = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Ground")
+        {
+            isOnGround = false;
+        }
+    }
+}
